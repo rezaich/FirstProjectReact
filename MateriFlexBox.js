70771d9519e0df9840336dd4ed52081f37f6f5ed
rezaich <ichsanireza@gmail.com>
2021-11-12 20:45:52 +0700

    burn

---
+++ MateriFlexBox.js
@@ -0,0 +1,20 @@
+import React,{Component} from "react";
+import { Text, View } from "react-native";
+
+
+class MateriFlexBox extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Materi Flex Box</Text>
+                <View />
+            </View>
+        )
+    }
+}
+
+// const MateriFlexBox = () => {
+//     return <Text>Materi react</Text>;
+// }
+
+export default MateriFlexBox;
